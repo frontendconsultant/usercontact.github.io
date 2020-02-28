c424a83330a1d4ce50b8d57af27d37449bf8ac0f
vijay-kumar-singh <vijaysingh.nl@gmail.com>
2020-02-28 18:24:59 +0100

    commit5

---
+++ build/precache-manifest.2ed71ac426f42c1c218500dde0bf0baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb52826b7e1c7b50c02fb6b1b48a615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf42899912f87ba5103",
+    "url": "/static/css/main.6d2b1e6e.chunk.css"
+  },
+  {
+    "revision": "1ae7b1be1c96f257e15c",
+    "url": "/static/js/2.93fef0cd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.93fef0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf42899912f87ba5103",
+    "url": "/static/js/main.e37e374b.chunk.js"
+  },
+  {
+    "revision": "8ea29d19dafb5bc1b3f3",
+    "url": "/static/js/runtime-main.0f4e7dc2.js"
+  }
+]);
